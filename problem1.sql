5ac765c4cd8038b80aebb6ae0be6bd9854861e2e
Adewale Gbadamosi <adewalegbadamosi@gmail.com>
2024-10-13 20:01:12 -0230

    Data inserted into the tables

--- problem1.sql
+++ problem1.sql
@@ -33,3 +33,38 @@ CREATE TABLE enrollments {
     enrollment_date DATE NOT NULL,
     PRIMARY KEY (student_id, course_id)
 };
+
+-- Insert data into the students table
+INSERT INTO students (first_name, last_name, email, school_enrollment_date)
+VALUES
+('Mark', 'Stone', 'mstone@gmail.com', '2022-09-01'),
+('Wally', 'Smith', 'wally.smith@gmail.com', '2021-09-01'),
+('Emily', 'Gee', 'emily.gee@gmail.com', '2023-01-15'),
+('Michael', 'Brown', 'michaelbrown@yahoo.com', '2020-09-01'),
+('Faith', 'Adegunna', 'faithadegunna@yahoo.com', '2022-05-01');
+
+-- Insert data into the professors table
+INSERT INTO professor (first_name, last_name, department)
+VALUES
+('Ahmed', 'Surkur', 'Computer Science'),
+('Marie', 'Curie', 'Physics'),
+('Emma', 'Isaac', 'Mathematics'),
+('David', 'Osho', 'Biology');
+
+-- Insert data into the courses table
+INSERT INTO courses (course_name, course_description, professor_id)
+VALUES
+('Physics 101', 'Introduction to Physics', 2),
+('Computer Science 101', 'Introduction to Computer Science', 1),
+('Mathematics 101', 'Introduction to Mathematics', 3);
+
+-- Insert data into the enrollments table
+INSERT INTO enrollments (student_id, course_id, enrollment_date)
+VALUES
+(1, 1, '2023-09-01'),
+(2, 1, '2023-09-02'),
+(3, 2, '2023-09-03'),
+(4, 3, '2023-09-04'),
+(5, 1, '2023-09-05');
+
+
